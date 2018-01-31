d59cae93cd3042c7919da3f1073f2a1b466c059f
Jon Lambson <jonlambson@gmail.com>
2018-01-30 19:45:10 -0700

    added o lot of css

--- src/index.js
+++ src/index.js
@@ -1,6 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+
+
+import './assets/css/bootstrap.min.css';
+import './assets/css/paper-kit.css?v=2.1.0';
+import './assets/css/demo.css';
+import './assets/css/nucleo-icons.css';
+
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
